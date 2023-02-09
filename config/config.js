a65af962ce6c513b77abeec5cef8559d247e848b
Ember96 <gnomeaddict96@gmail.com>
2023-02-08 23:44:59 -0800

    Initial commit

--- config/config.js
+++
@@ -1,2 +0,0 @@
-module.exports.SEED = 'JSI5A6P8D9J2SAPIDJSAIPDJSAIPDJASPI48413561#EWQETT**';
-module.exports.URI = 'mongodb+srv://dbUser:s3cr3t@cluster0.hsljv.mongodb.net/?retryWrites=true&w=majority';
